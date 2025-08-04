ff460dd1772c44cf1201aedbb39acdb5aaebe59b
Alexander-DA <alex.alexandrov200@gmail.com>
2025-08-04 16:32:24 +0300

    Added insert and delete methods

--- linked-list.ts
+++ linked-list.ts
@@ -23,10 +23,57 @@ class LinkedList<T> {
         this.length++;
     }
 
+
+    insertAt(value: T, pos: number) {
+        if(pos > -1 && pos < this.length && this.root) {
+            let current = this.root;
+            let index = 0;
+            let previous = current;
+            let node = new ListNode(value);
+
+            if (pos === 0) {
+                node.next = this.root;
+                this.root = node;
+            } else {
+                while (index++ < pos && current.next) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            this.length++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    removeAt(pos: number) {
+        if(pos > -1 && pos < this.length && this.root) {
+            let current = this.root;
+            let previous: ListNode<T> = current;
+            let index = 0;
+
+            if(pos === 0) {
+                this.root = current.next;
+            } else {
+                while (index++ < pos && current.next) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            this.length--;
+            return current;
+        } else {
+            return null;
+        }
+    }
+
     getNumberOfElements() {
         return this.length;
     }
-    
 
     print() {
         let current = this.root;
@@ -43,7 +90,7 @@ numberList.add(10);
 numberList.add(5);
 numberList.add(-3);
 
-console.log(numberList.getNumberOfElements());
+console.log(`Count of nodes: ${numberList.getNumberOfElements()}`);
 numberList.print();
 
 
